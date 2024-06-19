2795f14fc5a851551c25e5991c718fb1747e5ebf
Ivan Konderev <konderev@aheadworks.com>
2024-06-19 14:56:53 +0300

    Add gh-pages, link to Repo

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,11 +1,12 @@
 export default function Footer() {
+  const currentDate = new Date().getFullYear();
   return (
     <footer className="page-footer blue-grey lighten-1">
       <div className="footer-copyright">
         <div className="container">
-          © 2014 Copyright Text
-          <a className="grey-text text-lighten-4 right" href="#!">
-            More Links
+          © {currentDate} Copyright Text
+          <a className="grey-text text-lighten-4 right" href="https://github.com/kondzerau-ivan/shop-react" target="_blank" rel="noreferrer">
+            REPO
           </a>
         </div>
       </div>
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,7 +4,7 @@ export default function Header() {
       <nav className="blue-grey lighten-1">
         <div className="container">
           <div className="nav-wrapper">
-            <a href="#" className="brand-logo">
+            <a href="/" className="brand-logo">
               React Shop
             </a>
           </div>
